87856020c0020d9af293648e8928f0ebc1d16850
Maximiliano <143568715+Gatomaxxpro@users.noreply.github.com>
2024-04-17 22:48:29 -0700

    Add files via upload

---
+++ eva2_13_POLIMORFISMO/src/eva2_13_polimorfismo/Eva2_13_POLIMORFISMO.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_13_polimorfismo;
+
+/**
+ *
+ * @author MAX
+ */
+public class Eva2_13_POLIMORFISMO {
+
+    /**
+     *relacion entre 
+     */
+    public static void main(String[] args) {
+    
+        // TODO code application logic here
+        
+    }
+    public static void imprimirDatos(MostrarDt datos ){
+        
+    }
+}
+
---
+++ eva2_13_POLIMORFISMO/src/eva2_13_polimorfismo/persona.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package eva2_13_polimorfismo;
+
+/**
+ *
+ * @author MAX
+ */
+public class persona {
+    class Estudiante extends persona{
+    private String nmc;
+    public Estudiante (){
+    super();
+    nmc="";
+       
+    }
+
+    public String getNmc() {
+        return nmc;
+    }
+
+    public void setNmc(String nmc) {
+        this.nmc = nmc;
+    }
+
+    public Estudiante(String nmc) {
+        this.nmc = nmc;
+    }
+    @Override
+
+      
+    public String toString(){
+        return " "+nmc;
+    }
+    
+}
+}
